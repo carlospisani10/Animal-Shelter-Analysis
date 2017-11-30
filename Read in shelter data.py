81aa8b5fbd0432b6751c0911a6bbe95d5c3e1d10
ecogsdill <emily.cogsdill@gmail.com>
2017-11-29 19:11:46 -0600

    fixed Austin data file and added python script to read in data

---
+++ Read in shelter data.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+austin_data = pd.read_csv("../raw data/Austin.csv")
+louisville_data = pd.read_csv("../raw data/Louisville.csv")
+
+
+# In[8]:
+
+
+austin_data.head()
+louisville_data.head()
+
